0059b6a44fac1471ba2f4f5a851d408972764645
Wladislav161 <67539608+Wladislav161@users.noreply.github.com>
2020-12-20 12:15:27 +0300

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,23 @@
+create table table_abonent
+(
+    id         integer not null
+        constraint table_abonent_pk
+            primary key autoincrement,
+    first_name TEXT    not null,
+    midle_name TEXT    not null,
+    active     integer not null,
+    address    TEXT    not null
+);
+
+create table phones
+(
+    number integer not null
+        references table_abonent
+);
+
+create table table_group
+(
+    "group" TEXT not null
+        constraint table_group_table_abonent_first_name_fk
+            references table_abonent (first_name)
+);
