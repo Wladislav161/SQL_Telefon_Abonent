74af0577ccc0eae2e9cdb6af62333803718c90b6
Wladislav161 <67539608+Wladislav161@users.noreply.github.com>
2020-12-20 12:16:42 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -4,6 +4,7 @@ create table table_abonent
         constraint table_abonent_pk
             primary key autoincrement,
     first_name TEXT    not null,
+    last_name  TEXT    not null,
     midle_name TEXT    not null,
     active     integer not null,
     address    TEXT    not null
